4939933235b82c656e07a9dba50bc1fbfe651d6f
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2015-06-05 17:22:38 +0100

    Fixed string parsing. Next just need to route the paths somewhere useful.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
     char *data;
-    char *querystr;
+    char *querystr = malloc( 200 );
+    char *token;
+    char tmp = 's';
     int i;
+    int c = 0;
 
     printf("Content-Type: text/html;charset=utf-8\n\n");
     data = getenv("QUERY_STRING");
     if( data == NULL )
         printf("<p>Error! QUERY_STRING is NULL.</p>\n");
-    else if(i = sscanf(data,"path=%s", querystr)!=1)
+    else if( ( i = sscanf( data, "path=/%s", querystr ) ) != 1 )
         printf("<p>Error! Invalid data. Code: %d</p>\n", i);
     else {
 
         if(strcmp(querystr, "/") == 0)
             printf("{\"error\":\"No action specified.\", \"code\":\"ENOACT\"}\n");
         else {
-            printf("Page required: %s", querystr);
+            printf( "Page required: %s<br />\n", querystr );
+            printf( "<ul>\n" );
+
+            while( ( token = strsep( &querystr, "/" ) ) != 0 ) {
+                if( strcmp( token, "" ) != 0 ) {
+                    printf( "<li>Token %d is: '%s'</li>\n", c+1, token );
+                    c++;
+                }
+            }
+            printf( "</ul><br />\n" );
+            if( c == 1 ) {
+                tmp = '\0';
+            }
+            printf( "Total of %d token%c.<br />\n", c, tmp );
         }
     }
 
